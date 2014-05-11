ea038dc15713b70f6ace4e47c3f81eddbdde56e5
adamjanda <adam.janda@gmail.com>
2014-05-11 21:46:47 +0200

    Druga oficjalna wrzuta.

---
+++ public_html/js/slidingDiv.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(".slidingDiv").hide();
+    $(".show_hide").show();
+    $('.show_hide').click(function () {
+        $(".slidingDiv").slideToggle();
+    });
+});
+
+$(document).ready(function () {
+    $(".slidingDiv2").hide();
+    $(".show_hide2").show();
+    $('.show_hide2').click(function () {
+        $(".slidingDiv2").slideToggle();
+    });
+});
