16d6c18066cc3066e30028efbe042693c4d27e09
GrzKon <facesyf@gmail.com>
2014-05-25 18:01:16 +0200

    zmiana w ścieżce

--- public_html/js/path_to/dodaj.php
+++ public_html/js/path_to/dodaj.php
@@ -1,7 +1,13 @@
 <?php
-    define ('SITE_ROOT', realpath(dirname(__FILE__) . '/path_to'));
-    $tmp_file_name = $_FILES['Filedata']['tmp_name'];
-    $ok=move_uploaded_file($tmp_file_name,SITE_ROOT.$_FILES['Filedata']['name']);
-    // This message will be passed to 'oncomplete' function
-    echo $ok ? "OK" : "FAIL";
-?>
+
+      $fileTypes = array('bmp'); // Allowed file extensions
+      define ('SITE_ROOT', realpath(dirname(__FILE__) . '/path_to'));
+      $tmp_file_name = $_FILES['Filedata']['tmp_name'];
+      $fileParts = pathinfo($_FILES['Filedata']['name']);
+        if (in_array(strtolower($fileParts['extension']), $fileTypes)) {
+            move_uploaded_file($tmp_file_name,SITE_ROOT.$_FILES['Filedata']['name']);
+             echo 'ok';
+            } else {
+    echo 'Invalid file type.';
+    }
+    ?>
